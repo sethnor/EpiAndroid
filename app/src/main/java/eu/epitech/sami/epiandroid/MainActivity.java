1ea375a0bbc3721987457660ca36722d6a67ac07
sethnor <sami.skalli@epitech.eu>
2016-01-25 14:14:42 +0100

    bouton connexion

--- app/src/main/java/eu/epitech/sami/epiandroid/MainActivity.java
+++ app/src/main/java/eu/epitech/sami/epiandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package eu.epitech.sami.epiandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,10 +26,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                System.out.println("fdp");
             }
         });
-    }
+
+        final Button button = (Button) findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent index = new Intent(MainActivity.this, IndexActivity.class);
+
+                startActivity(index);
+            }
+        });    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
