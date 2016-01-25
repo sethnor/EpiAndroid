4365a2ceff939573a26fa7f94fcd34b3b43b0fd8
sethnor <sami.skalli@epitech.eu>
2016-01-25 14:39:20 +0100

    api beginning

---
+++ app/src/main/java/eu/epitech/sami/epiandroid/EpiRestClient.java
@@ -0,0 +1,22 @@
+package eu.epitech.sami.epiandroid;
+
+import com.loopj.android.http.*;
+/**
+ * Created by Sami on 25/01/2016.
+ */
+public class EpiRestClient {
+    private static final String     BASE_URL = "https://epitech-api.herokuapp.com/";
+    private static AsyncHttpClient  client = new AsyncHttpClient();
+
+    public static void      get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getUrl(url), params, responseHandler);
+    }
+
+    public static void      post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getUrl(url), params, responseHandler);
+    }
+
+    private static String   getUrl(String url) {
+        return BASE_URL + url;
+    }
+}
---
+++ app/src/main/java/eu/epitech/sami/epiandroid/EpiRestClientUsage.java
@@ -0,0 +1,8 @@
+package eu.epitech.sami.epiandroid;
+
+/**
+ * Created by Sami on 25/01/2016.
+ */
+public class EpiRestClientUsage {
+    
+}
